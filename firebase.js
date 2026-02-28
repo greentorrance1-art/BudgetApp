e0a5aeef173505d303f15999ceb5ec02f1d83a76
greentorrance1-art <greentorrance1@gmail.com>
2026-02-28 14:35:39 -0500

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAwkLkbyuiuHaMGpU8pJ5DsWi33i44Ljv4",
+  authDomain: "production-mode-290db.firebaseapp.com",
+  projectId: "production-mode-290db",
+  storageBucket: "production-mode-290db.firebasestorage.app",
+  messagingSenderId: "365690860963",
+  appId: "1:365690860963:web:84ad0e1731fd3aa4ccd616",
+  measurementId: "G-G5HK6VM4L0"
+};
